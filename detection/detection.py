273453e8058769c4abfe8d6db413853c5a249d45
Sebastien Girard <sebastiengrd@gmail.com>
2020-04-26 21:53:28 -0400

    obtained a good binary image in python with denoising technique and adaptive thresholding

--- detection/detection.py
+++ detection/detection.py
@@ -8,21 +8,55 @@ imagesPath = listdir(imagesDir)
 
 print(imagesPath)
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+
 for imgName in imagesPath:
     img = cv2.imread(imagesDir + imgName)
 
+    img = image_resize(img, width=500)
 
-    img = cv2.blur(img, (5, 5))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
+    denoised = cv2.fastNlMeansDenoising(gray, h=10, templateWindowSize=7, searchWindowSize=21)
 
-    thresh0 = cv2.adaptiveThreshold(s, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    thresh1 = cv2.adaptiveThreshold(v, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    thresh2 = cv2.adaptiveThreshold(v, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    thresh = cv2.bitwise_or(thresh0, thresh1)
+    th = cv2.adaptiveThreshold(denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,9,7)
 
-    cv2.imshow("gray", thresh)
+    
+
+
+
+    cv2.imshow("image", th)
     cv2.waitKey(0)
+
 
 cv2.destroyAllWindows()
